6ad1fc23a04e50dda471f755213e90b04a2a7ffc
rishi <rishirajsingh.thought@gmail.com>
2021-04-14 19:33:22 +0530

    third commit

---
+++ apps/techapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+# Create your models here.
+class TechItem(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_image = models.ImageField(upload_to=None, height_field=None, width_field=None, )
+    item_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.item_name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=150)
+    mobile_number = models.CharField(blank=True , max_length=20)
+    email = models.EmailField(max_length=200)
+    employee_team = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Request(models.Model):
+    tech_item = models.ForeignKey(TechItem, on_delete=models.CASCADE)
+    employee_name = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.tech_item.name

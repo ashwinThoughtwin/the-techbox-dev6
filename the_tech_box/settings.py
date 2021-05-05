f5b3b00cdd62d4c95f4e3570c17fd6b31dc27672
rishi <rishirajsingh.thought@gmail.com>
2021-05-05 17:09:16 +0530

    static

--- the_tech_box/settings.py
+++ the_tech_box/settings.py
@@ -42,7 +42,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ybxh0d5jj1$5jln4wra06rw_!ab1+yiks!1v@s4bw%h-cwlg*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['rishiraj-thetechbox.herokuapp.com', '127.0.0.1']
 
